593b39935dbf57e7d9b7a7af065ae299026e0c37
ShuvoCSE176 <asifshuvo119181@gmail.com>
2020-09-27 04:06:41 +0600

    Revision done

--- index.js
+++ index.js
@@ -1,9 +1,44 @@
 const express = require('express');
+const cors=require('cors');
+const bodyParser = require('body-parser');
 
 const app = express();
 
+app.use(cors());
+app.use(bodyParser.json());
+
+const users=['shuvo','shawon','zobayer','sojol','Ragib']
+
 app.get('/', (req, res) =>{
-    res.send('I can start a node app')
+
+    const fruit={
+        products:'ada',
+        price:250
+    }
+    res.send(fruit)
+});
+
+app.get('/fruits/banana',(req, res) =>{
+    res.send({fruit:'banana',quantity:10000,price:10000})
 })
+
+app.get ('/users/:id', (req, res)=>{
+    const id= (req.params.id);
+    // console.log(req.query.sort)
+    const name=users[id];
+    res.send({id,name});
+})
+
+//post 
+
+app.post('/addUser',(req, res)=>{
+    // console.log('data received',req.body);
+        const user=req.body;
+        user.id=31;
+        res.send(user);
+
+})
+
+
 
 app.listen(5000 , ()=> console.log('Listening to port no 5000'))
